4ec456a38b24d63ddf137ab1dcc0fa7c31e912c7
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-07-25 20:14:31 +0530

    Task-01 Completed

--- chapter-02 Igniting App/code/app.js
+++ chapter-02 Igniting App/code/app.js
@@ -3,9 +3,32 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading1 = React.createElement("h1" , {id:"heading1"} , "Hello World");
-const heading2 = React.createElement("h2" , {id:"heading2"} , "Learning React...");
-const heading3 = React.createElement("h3" , {id:"heading3"} , "Let us play with React !!!!");
+// const heading1 = React.createElement("h1" , {id:"heading1"} , "Hello World");
+// const heading2 = React.createElement("h2" , {id:"heading2"} , "Learning React...");
+// const heading3 = React.createElement("h3" , {id:"heading3"} , "Let us play with React !!!!");
 
-const createDiv = React.createElement("div" , {className:"creatediv"} , [heading1 , heading2 , heading3])
-root.render(createDiv);
+// const createDiv = React.createElement("div" , {className:"creatediv"} , [heading1 , heading2 , heading3])
+// root.render(createDiv);
+
+// Let's play 
+// TASK-01
+
+  <div class="parent">
+    <div id="child">
+        <h1>This is heading1</h1>
+        <h2>This is heading2</h2>
+    </div>
+</div> 
+
+
+const heading = React.createElement(
+  "div",
+  { className: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is heading1"),
+    React.createElement("h2", {}, "This is heading2"),
+  ])
+);
+
+
+root.render(heading);
