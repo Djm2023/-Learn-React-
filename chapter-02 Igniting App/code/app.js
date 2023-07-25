3bee15cb5623f3818f287561a946d5d9313da06d
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-07-25 20:05:24 +0530

    Updated Code

--- chapter-02 Igniting App/code/app.js
+++ chapter-02 Igniting App/code/app.js
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "heading1" }, "Hello World");
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading2" },
-  "Learning React"
-);
-const heading3 = React.createElement(
-  "h3",
-  { id: "heading3" },
-  "Let us play with React...."
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const createDiv = React.createElement(
-  "div",
-  {
-    style: {
-      color: "brown",
-    },
-    className: "create",
-  },
-  [heading1, heading2, heading3]
-);
+const heading1 = React.createElement("h1" , {id:"heading1"} , "Hello World");
+const heading2 = React.createElement("h2" , {id:"heading2"} , "Learning React...");
+const heading3 = React.createElement("h3" , {id:"heading3"} , "Let us play with React !!!!");
 
-//  console.log(heading) return object
-// react create element always give objects
-const root = ReactDOM.createRoot(document.getElementById("root")); // Create Root element for the React DOM
+const createDiv = React.createElement("div" , {className:"creatediv"} , [heading1 , heading2 , heading3])
 root.render(createDiv);
